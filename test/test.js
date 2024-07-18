db6fa1fd5d2263795f46a92a564fe7cacd69f095
Vik <vyagnov@gmail.com>
2024-07-18 13:19:13 -0400

    basic tests

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const mariadb = require("mariadb")
 let db = null
 
 async function connect() {
-    console.info("Connecting to DB...");
+    //console.info("Connecting to DB...");
     db = mariadb.createPool({
         host: process.env["DATABASE_HOST"],
         user: process.env["DATABASE_USER"],
@@ -15,7 +15,7 @@ async function connect() {
     const conn = await db.getConnection();
     try {
         await conn.query("SELECT 1");
-        console.info("DB Connected!!!");
+        //console.info("DB Connected!!!");
     } finally {
         await conn.end();
     }
@@ -35,11 +35,50 @@ describe("GET /", () => {
         //
         //console.log(res)
         //expect(res.body).toBe({});
-        expect(res.text).toBe("Hello!");
+        expect(res.text).toContain("Pie Chart");
+    });
+});
+
+describe("GET /get/workers", () => {
+    it("Should return JSON of Taksk", async () => {
+        const res = await request(process.env["API_HOST"]).get("/get/workers").send({
+            "is_complete": null,
+            "location_ids": [],
+            "worker_ids": [],
+        }).set('Content-Type', 'application/json');
+        expect(res.statusCode).toBe(200);
+        expect(res.text.length).toBeGreaterThan(0);
+
+    });
+});
+
+describe("GET /get/locations", () => {
+    it("Should return JSON of Taksk", async () => {
+        const res = await request(process.env["API_HOST"]).get("/get/locations").send({
+            "is_complete": null,
+            "location_ids": [],
+            "worker_ids": [],
+        }).set('Content-Type', 'application/json');
+        expect(res.statusCode).toBe(200);
+        expect(res.text.length).toBeGreaterThan(0);
+
     });
 });
 
 describe("POST /tasks/worker", () => {
+    it("Should return JSON of Taksk", async () => {
+        const res = await request(process.env["API_HOST"]).post("/tasks/worker").send({
+            "is_complete": null,
+            "location_ids": [],
+            "worker_ids": [],
+        }).set('Content-Type', 'application/json');
+        expect(res.statusCode).toBe(200);
+        //expect(res.body.length).toBeGreaterThan(0);
+        expect(res.body.result).toBe("OK");
+    });
+});
+
+describe("POST /tasks/location", () => {
     it("Should return JSON of Taksk", async () => {
         const res = await request(process.env["API_HOST"]).post("/tasks/worker").send({
             "is_complete": null,
